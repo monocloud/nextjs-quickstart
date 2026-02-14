da9b7dd9224666670e651c5e6bf5ed76b1824d8e
lakshayysaini <lakshayysaini@gmail.com>
2026-02-15 03:10:00 +0530

    fix: changed the code from using single monocloud instance to individual functions

--- src/monocloud.ts
+++
@@ -1,3 +0,0 @@
-import { MonoCloudNextClient } from "@monocloud/auth-nextjs";
-
-export const monoCloud = new MonoCloudNextClient();
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,9 @@
-import { monoCloud } from "@/monocloud";
+import { authMiddleware } from "@monocloud/auth-nextjs";
 
-export default monoCloud.authMiddleware();
+export default authMiddleware();
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
